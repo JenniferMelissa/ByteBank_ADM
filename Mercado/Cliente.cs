b742247448c985ba91fecee60a2c8c2ce3b853f5
Jennifer Melissa Martins de Sales <jennifer.sales@geap.com.br>
2022-11-17 09:59:04 -0300

    Classe Object C# - Transcrição

---
+++ Mercado/Cliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mercado
+{
+    public class Cliente
+    {
+
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public string? Email { get; set; }
+        public int Idade { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Nome: {this.Nome},\n" +
+                   $"Email: {this.Email},\n" +
+                   $"Idade: {this.Idade},\n" +
+                   $"Cpf: {this.Cpf}";
+        }
+    }
+}
---
+++ Mercado/Program.cs
@@ -0,0 +1,18 @@
+﻿using Mercado;
+
+Cliente cliente = new Cliente();
+cliente.Nome = "André";
+cliente.Cpf = "111.444.666-88";
+cliente.Email = "andre@email.com";
+cliente.Idade = 23;
+
+
+
+Console.WriteLine(cliente.Nome);
+Console.WriteLine(cliente.Cpf);
+Console.WriteLine(cliente.Idade);
+
+//Exibe tudo
+Console.WriteLine(cliente.ToString());
+//OU
+Console.WriteLine(cliente);
